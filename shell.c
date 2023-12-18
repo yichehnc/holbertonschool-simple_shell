87cd90740b8e70ecaadc42f1ce905176c9aa84c7
Eche <echyth@yahoo.com>
2023-12-18 22:56:57 +1000

    add initial shell.c and shell.h

---
+++ shell.c
@@ -0,0 +1,11 @@
+/**
+ * main - entry point to the program
+ * @argc: argument count
+ * @argv: array of string literals passed as argument to main
+ * Return: 0 if no error, else a value other than zero
+ */
+int main(int argc, char const *argv[])
+{
+	printf("Hello shell\n");
+	return 0;
+}
