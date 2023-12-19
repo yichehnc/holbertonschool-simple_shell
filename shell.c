80907d0d7e8184fc03487ec3d0f73b45c4cd55bd
yichehnc <yichenc2017@gmail.com>
2023-12-19 09:35:52 +0800

    update shell.c

--- shell.c
+++ shell.c
@@ -1,11 +1,35 @@
-/**
- * main - entry point to the program
- * @argc: argument count
- * @argv: array of string literals passed as argument to main
- * Return: 0 if no error, else a value other than zero
- */
-int main(int argc, char const *argv[])
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+#include "shell.h"
+void shell_loop()
 {
-	printf("Hello shell\n");
-	return (0);
+        /*
+        Read: Read command from standard input
+        Parse: Separate command into a program and arguments
+        Execute: Run the parsed command
+        */
+        char *line;
+        char **commands;
+        int bytes_read;
+        int status;
+        do
+        {
+                printf("$ ");
+                line = _read_line();
+                if (!(*line))
+                        break;
+                commands = _split_string(line);
+                status = _execute_command(commands);
+                free(line);
+                free(commands);
+        } while (status);
+}
+int main(int argc, char **argv)
+{
+        shell_loop();
+        return (EXIT_SUCCESS);
 }
