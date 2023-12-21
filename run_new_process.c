4c6e1b3d3727f3b246734f634307ec9b1ab2fa3e
Eche <echyth@yahoo.com>
2023-12-21 23:23:01 +1000

    fix: add functionality to check if command file exists

--- run_new_process.c
+++ run_new_process.c
@@ -1,35 +1,49 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <stddef.h>
+#include "shell.h"
 
 int run_new_process(char **args)
 {
 	pid_t pid;
 	int status;
+	char *filename = args[0];
+	char *filepath = NULL;
 
-	pid = fork();
-
-	if (pid == 0)
+	if (strchr(args[0], '/'))
 	{
-		if (execvp(args[0], args) == -1)
-		{
-			perror("error _execute_command: child process");
-		}
-		exit(EXIT_FAILURE);
-	}
-	else if (pid < 0)
-	{
-		perror("error in _execute_command: forking");
+		filepath = args[0];
 	}
 	else
 	{
-		do
+		filepath = findFileInPath(filename);
+	}
+
+	if (filepath != NULL)
+	{
+		pid = fork();
+
+		if (pid == 0)
 		{
-			waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+			if (execvp(filepath, args) == -1)
+			{
+				perror("error _execute_command: child process");
+			}
+			exit(EXIT_FAILURE);
+		}
+		else if (pid < 0)
+		{
+			perror("error in _execute_command: forking");
+		}
+		else
+		{
+			do
+			{
+				waitpid(pid, &status, WUNTRACED);
+			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		}
+	}
+	else
+	{
+		fprintf(stderr, "%s: %d: %s: not found\n", "./shell", 1, args[0]);
+		exit(127);
 	}
 
 	return (-1);
