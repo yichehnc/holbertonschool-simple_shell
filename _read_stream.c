2b65f590f3a8601bf23489da4ccf0546f492b07e
Eche <echyth@yahoo.com>
2023-12-23 21:03:06 +1000

    fix: updates for coding style errors in _read_stream.c

--- _read_stream.c
+++ _read_stream.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define BUFSIZE 1024;
+#define BUFSIZE 1024
 
+/**
+ * _read_stream - Read a line from standard input using dynamic memory
+ * allocation.
+ *
+ * Description:
+ * This function reads characters from standard input and dynamically allocates
+ * memory to store the line. The buffer size starts at BUFSIZE and grows if
+ * needed.
+ * It returns the read line as a dynamically allocated string or NULL on error
+ * or end of file.
+ *
+ * Return:
+ * A dynamically allocated string containing the read line, or NULL on error
+ * or end of file.
+ */
 char *_read_stream()
 {
-	int bufsize, i, ch;
-	char *line;
-	bufsize = BUFSIZE;
-	i = 0;
-
-	line = malloc(bufsize * sizeof(char));
+	int bufsize = BUFSIZE, i = 0, ch;
+	char *line = malloc(bufsize * sizeof(char));
 
 	if (line == NULL)
 	{
 		perror("memory allocation failed in _read_stream\n");
-		return NULL;
+		return (NULL);
 	}
 
 	while ((ch = getchar()) != EOF)
@@ -35,11 +46,11 @@ char *_read_stream()
 			if (line == NULL)
 			{
 				fprintf(stderr, "reallocation error in read_stream");
-				return NULL;
+				return (NULL);
 			}
 		}
 	}
 
 	free(line);
-	return NULL;
+	return (NULL);
 }
