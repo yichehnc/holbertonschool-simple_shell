5aa985401628dbf3a297dcc493b0d85fcfcae793
Eche <echyth@yahoo.com>
2023-12-23 21:06:14 +1000

    fix: updates for coding style errors in _execute_args.c

--- _execute_args.c
+++ _execute_args.c
@@ -6,6 +6,23 @@
 #include <stddef.h>
 #include "shell.h"
 
+/**
+ * _execute_args - Execute a command or a built-in function based on arguments.
+ * @args: Array of strings representing the command and its arguments.
+ *
+ * Description:
+ * This function evaluates the command specified in the args array and either
+ * executes a built-in function or a new process. It checks for built-in
+ * functions
+ * such as cd, help, exit, and env. If the command is a built-in function, it
+ * calls the corresponding function; otherwise, it runs a new process.
+ *
+ * Parameters:
+ * @args: Array of strings representing the command and its arguments.
+ *
+ * Return:
+ * Returns the exit status of the executed command or built-in function.
+ */
 int _execute_args(char **args)
 {
 	char *builtin_funcs_list[] = {"cd", "help", "exit", "env"};
