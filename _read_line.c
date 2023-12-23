e8bc9e25383be0af2b27536268f6e2e4b6c145ed
Eche <echyth@yahoo.com>
2023-12-23 20:56:02 +1000

    fix: updates for coding style errors in _read_line.c

--- _read_line.c
+++ _read_line.c
@@ -2,6 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 #define BUFSIZE 1024
+
+/**
+ * _read_line - Read a line from standard input.
+ *
+ * Description:
+ * This function reads a line from standard input using the getline function.
+ * It allocates memory for the line and returns it. If an error occurs or
+ * the end of file is reached, it returns NULL.
+ *
+ * Return:
+ * A dynamically allocated string containing the read line, or NULL on error
+ * or end of file.
+ */
 char *_read_line(void)
 {
 	char *line = NULL;
@@ -12,14 +25,14 @@ char *_read_line(void)
 		free(line);
 		if (feof(stdin))
 		{
-			return NULL;
+			return (NULL);
 		}
 		else
 		{
 			perror("error while reading line from stdin");
-			return NULL;
+			return (NULL);
 		}
 	}
 
-	return line;
+	return (line);
 }
